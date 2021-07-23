f434cfd9956b9c619c47b5f802ef46f0cab32c8c
Manridev <jmanrique@kerajet.com>
2021-07-23 16:07:56 +0200

    Evento clik (Eventos en general)

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,11 +1,14 @@
 import React from 'react';
+const handledAdd = (e) => {
+    console.log(e)
+}
 
 const CounterApp = ({ value }) => {
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{value}</h2>
-
+            <button onClick={handledAdd}>+1</button>
         </>
     )
 }
@@ -13,6 +16,6 @@ const CounterApp = ({ value }) => {
 
 CounterApp.ReactPropTypes = {
     value: {}
-}
+    }
 
 export default CounterApp;
