7f9025c09558ac12a41e73692f55d118b5e2df5a
Manridev <jmanrique@kerajet.com>
2021-07-23 17:28:16 +0200

    handledSusbtract and handledReset

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,20 +1,22 @@
 import React, { useState } from 'react'
 
 
-const CounterApp = ({ value }) => {
-    const [counter, setCounter] = useState(1)
+const CounterApp = ({ value = 10 }) => {
+    const [counter, setCounter] = useState(value)
 
-    const handledAdd = () => {
-        setCounter(counter + 1)
-        // setCounter((c) => c + 1)
+    const handledAdd = () => setCounter(counter + 1)
 
-    }
+    const handledSubtrac = () => setCounter(counter - 1)
+
+    const handledReset = () => setCounter(value)
 
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{counter}</h2>
             <button onClick={handledAdd}>+1</button>
+            <button onClick={handledSubtrac}>-1</button>
+            <button onClick={handledReset}>Reset</button>
         </>
     )
 }
