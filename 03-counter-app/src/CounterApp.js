4f75561e2287f28151ee2eb72626c9de528d2d74
Manridev <jmanrique@kerajet.com>
2021-07-23 16:33:51 +0200

    useState - Hook

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,21 +1,26 @@
-import React from 'react';
-const handledAdd = (e) => {
-    console.log(e)
-}
+import React, { useState } from 'react'
+
 
 const CounterApp = ({ value }) => {
+    const [counter, setCounter] = useState(1)
+
+    const handledAdd = () => {
+        setCounter(counter + 1)
+        // setCounter((c) => c + 1)
+
+    }
+
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{value}</h2>
+            <h2>{counter}</h2>
             <button onClick={handledAdd}>+1</button>
         </>
     )
 }
 
-
 CounterApp.ReactPropTypes = {
     value: {}
-    }
+}
 
 export default CounterApp;
